3e14d0202b7605261926e531a54d4160dfe9fb06
Alex <alexmore080@gmail.com>
2022-07-27 16:00:48 -0500

    added event listener on start quiz button

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var secondsLeft = 10;
+var start = document.getElementById("startbutton");
+start.addEventListener("click",gameStart);
+
+function gameStart(){
+    console.log("I am being clicked");
+}
