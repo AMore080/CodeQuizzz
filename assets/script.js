2352941b1f266b2ce97bb38ff05201dda000b90c
Alex <alexmore080@gmail.com>
2022-07-29 16:53:14 -0500

    start button will hide the homepage when clicked

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,15 @@
 var secondsLeft = 10;
 var start = document.getElementById("startbutton");
+var homePage = document.getElementById('containerHead');
+var questionsCont = document.getElementById('questionContainer');
+
+
+
 start.addEventListener("click",gameStart);
 
 function gameStart(){
     console.log("I am being clicked");
+    homePage.classList.add('hide');
+    questionsCont.classList.remove('hide');
+
 }
