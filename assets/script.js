d3eb8075faacf55fdbd9d69cb733b7289ec6f810
Alex <alexmore080@gmail.com>
2022-07-31 17:51:24 -0500

    showQuestions function displays questions

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,35 @@ var secondsLeft = 10;
 var start = document.getElementById("startbutton");
 var homePage = document.getElementById('containerHead');
 var questionsCont = document.getElementById('questionContainer');
-
+var questionString = document.getElementById('questions');
+var btnElement = document.getElementsByClassName('btn');
+var shuffledQuestions, currentQuestionIndex
 
 
 start.addEventListener("click",gameStart);
+
+
 
 function gameStart(){
     console.log("I am being clicked");
     homePage.classList.add('hide');
     questionsCont.classList.remove('hide');
-
+    showQuestions();
 }
+
+function showQuestions(){
+    questionString.innerHTML = questionsArr.question
+    document.getElementsByClassName('btn').innerHTML = " ";
+    btnElement.textcontent = questionsArr.answers.text
+    }
+
+
+
+
+    const questionsArr = 
+        {
+        question: "Which option is the correct way to close a tag?",
+        answers: [
+            {text: '</>'}
+        ]
+    }
